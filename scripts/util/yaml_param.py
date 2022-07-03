9ba71106837277398a613d2a4fc29eafc601cf99
YutaHoda <180447001@ccmailg.meijo-u.ac.jp>
2022-07-03 16:42:09 +0900

    add adjust_time_stamp.py

--- scripts/util/yaml_param.py
+++ scripts/util/yaml_param.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import copy
+
 class YamlParam:
     def __init__(self):
         self.separate_time_stamp = ""
@@ -25,3 +29,31 @@ class YamlParam:
         self.save_figures = ""
         self.save_extension_type = ""
         self.save_dataframe = ""
+
+        self.direct_vel = ""
+        self.vel_colmn = ""
+
+        self.df_temp = pd.DataFrame()
+        self.df = pd.DataFrame()
+
+# ##YamlParamが使えない
+# class UnitExtraction(YamlParam):
+    def extract_time(self):
+        if self.separate_time_stamp == True:
+            self.df['time'] = self.df_temp.iloc[:, self.secs_stamp_column] + self.df_temp.iloc[:, self.nsecs_stamp_column] / 10**9
+        else:
+            self.df['time'] = self.df_temp.iloc[:, self.stamp_column]
+
+
+    def extract_vel(self):
+        if self.separate_time_stamp == True:
+            self.df['vel'] = self.df_temp.iloc[:, self.vel_colmn]
+        else:
+            for i in range(self.df):
+                if i==0:
+                    continue
+                dis_x = self.df_temp.iloc[i, self.x_column] - self.df_temp.iloc[i-1, self.x_column]
+                dis_y = self.df_temp.iloc[i, self.y_column] - self.df_temp.iloc[i-1, self.y_column]
+                dis_z = self.df_temp.iloc[i, self.z_column] - self.df_temp.iloc[i-1, self.z_column]
+                del_time = self.df.loc[i, 'time'] - self.df.loc[i-1, 'time']
+                self.df.at[i,'vel'] = np.sqrt(pow(dis_x,2) + pow(dis_y,2) + pow(dis_z,2))/ del_time # [m/s]
